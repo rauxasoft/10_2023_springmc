2d30a5e73a10dd1e87fa08a32564c3ad5d605cee
Jordi <jordia4@gmail.com>
2023-10-19 20:19:32 +0200

    se incluye el primer controlador

---
+++ src/main/java/com/rauxasoft/gestionproductos/backend/presentation/restcontrollers/ProductoController.java
@@ -0,0 +1,29 @@
+package com.rauxasoft.gestionproductos.backend.presentation.restcontrollers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rauxasoft.gestionproductos.backend.business.model.Producto;
+import com.rauxasoft.gestionproductos.backend.business.services.ProductoServices;
+
+@RestController
+public class ProductoController {
+
+	@Autowired
+	private ProductoServices productoServices;
+	
+	@GetMapping("/productos")
+	public List<Producto> getAll(){
+		return productoServices.getAll();
+	}
+	
+	@GetMapping("/productos/{id}")
+	public Producto read(@PathVariable Long id) {
+		return productoServices.read(id);
+	}
+	
+}
